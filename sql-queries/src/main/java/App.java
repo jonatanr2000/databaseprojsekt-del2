154e15a278f2cdb3d24d58e074d624cd012f668f
tordstr <tordstr@stud.ntnu.no>
2021-03-25 10:28:50 +0100

    dont really know what i did here, but sure was important

--- sql-queries/src/main/java/App.java
+++ sql-queries/src/main/java/App.java
@@ -1,4 +1,4 @@
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -16,13 +16,14 @@ public class App {
 
     /**
      * Prints the text in a slow fashion, adds a nice feel.
+     *
      * @param text String to be printed out.
      * @throws InterruptedException if timeout is interrupted.
      */
     public void print(String text) throws InterruptedException {
         for (int i = 0; i < text.length(); i++) {
             System.out.print(text.charAt(i));
-            TimeUnit.MILLISECONDS.sleep((long) 100); // in milliseconds
+            TimeUnit.MILLISECONDS.sleep(100); // in milliseconds
         }
         System.out.println();
     }
@@ -30,52 +31,66 @@ public class App {
 
     /**
      * runs the application
+     *
      * @throws InterruptedException if the print function is interrupted.
      */
     public void run() throws InterruptedException, SQLException {
-        print("Welcome to low budget PIAZZA \n " +
+        print("Welcome to low budget PIAZZA \n" +
                 "Please log in.");
-        while (piazzaCtrl.user == null) {
-            print("Email: ");
-            String email = scanner.nextLine().trim();
-            print("Password: ");
-            String password = scanner.nextLine().trim();
-            print("logging on with, email: " + email + " and password " + password);
-            user = piazzaCtrl.login(email, password);
-            makePost.setUser(user);
-        }
-
-        print("Welcome to the main hub.");
         String action = "";
-        while (!action.matches("log out")) {
-            print("create post \t view posts \t statistics \t log out");
-            print("What do you want to do?");
+        while (!action.matches("exit")) {
+            print("Type 'exit' if you want to close the program");
+            if (piazzaCtrl.user == null) {
+                login();
+            } else {
+                print("Welcome to the main hub.");
+                while (!action.matches("log out") && !action.matches("exit")) {
+                    print("create post \t view posts \t statistics \t log out");
+                    print("What do you want to do?");
 
-            action = scanner.nextLine();
-            switch(action) {
-                case "create post": {
-                    this.create_post();
+                    action = scanner.nextLine();
+                    switch (action) {
+                        case "create post": {
+                            this.create_post();
+                        }
+                        break;
+                        case "view posts": {
+                            this.view_threads();
+                        }
+                        break;
+                        case "statistics": {
+                            this.getStatistics();
+                        }
+                        break;
+                        case "log out":
+                        case "exit": {
+                            //Do nothing
+                        }
+                        break;
+                        default:
+                            print("Sorry I did not understand that");
+                    }
                 }
-                break;
-                case "view posts": {
-                    this.view_threads();
-                }
-                break;
-                case"statistics": {
-                    piazzaCtrl.getPosts();
-                }
-                break;
-                case "log out": {
-                    //Do nothing
-                }
-                break;
-                default:
-                    print("Sorry I did not understand that");
+                print("Logging out, good bye world");
             }
-
         }
-        print("Logging out, good bye world");
+    }
 
+    private void getStatistics() throws InterruptedException {
+        List<String> stats = piazzaCtrl.getStatistics();
+        for (String line : stats) {
+            print(line);
+        }
+    }
+
+    private void login() throws InterruptedException {
+        print("Email: ");
+        String email = scanner.nextLine().trim();
+        print("Password: ");
+        String password = scanner.nextLine().trim();
+        print("logging on with, email: " + email + " and password " + password);
+        user = piazzaCtrl.login(email, password);
+        makePost.setUser(user);
     }
 
     private void view_threads() throws InterruptedException {
@@ -90,12 +105,12 @@ public class App {
                 view_thread(id);
             } else if (action.contains("search:")) {
                 String searchText = action.substring(7);
-               // List<Integer> indexes = piazzaCtrl.search(searchText);
-                List<Integer> indexes = Arrays.asList(1,4, 5, 10);
+                // List<Integer> indexes = piazzaCtrl.search(searchText);
+                List<Integer> indexes = Arrays.asList(1, 4, 5, 10);
 
                 makePost.showThreads(indexes);
-            } else if (action.matches("go_back")){
-                //pass
+            } else if (action.matches("go_back")) {
+                print("going back");
             } else {
                 print("Sorry I did not understand that.");
             }
@@ -103,15 +118,19 @@ public class App {
     }
 
     private void view_thread(int id) throws InterruptedException {
+        piazzaCtrl.view(user.email, id);
         int postID = makePost.showPostsInThread(id);
         String action = "";
         while (!action.matches("go_back")) {
-            print("go_back \t make_reply");
+            print("go_back \t make_reply \t like<id>");
             action = scanner.nextLine();
             if (action.contains("make_reply")) {
                 make_reply(postID);
-            }else if (action.matches("go_back")) {
-                //pass
+            } else if (action.contains("like")) {
+                Integer likeID = Integer.parseInt(action.substring(4));
+                piazzaCtrl.regLike(user.email, likeID);
+            } else if (action.matches("go_back")) {
+                print("going back");
             } else {
                 print("Sorry I did not understand that");
             }
@@ -121,16 +140,16 @@ public class App {
     private void make_reply(int id) throws InterruptedException {
         print("text: ");
         String text = scanner.nextLine();
-        String colour = "";
+        String colour;
         if (user.isInstrucor) {
             colour = "orange";
         } else {
             colour = "green";
         }
-        makePost.makePost(text, colour, "reply", id, user.email);
+        int postID = makePost.makePost(text, colour, "reply", id, user.email);
 
-        print("creating reply:"  + " " + Integer.toString(id) + " " + " " + text);
-        piazzaCtrl.checkReply(id);
+        print("creating reply:" + " " + postID + " " + " " + text);
+        piazzaCtrl.checkReply(id, user);
     }
 
     /**
@@ -139,18 +158,21 @@ public class App {
     private void create_post() throws InterruptedException, SQLException {
         makePost.showFolders();
         print("Please choose folder by id");
-        int folderid = Integer.parseInt(scanner.nextLine());
+        int folderId = Integer.parseInt(scanner.nextLine());
         print("Title: ");
         String title = scanner.nextLine();
         print("text: ");
         String text = scanner.nextLine();
 
-        makePost.makeThread(title, folderid);
+        makePost.makeThread(title, folderId);
         makePost.makePost(text, "red", "post", makePost.getThreadIdLatest(), user.email);
         print("Please enter the tags separated by 'space'.");
         String[] tags = scanner.nextLine().toLowerCase(Locale.ROOT).split(" ");
-        makePost.connectTagsAndPost(makePost.getPostIdLatest(), tags);
-        print("creating :"  + " " + Integer.toString(folderid) + " " + title + " " + text);
+        boolean madeTags = makePost.connectTagsAndPost(makePost.getPostIdLatest(), tags);
+        if (!madeTags) {
+            print("Was unable to connect the tags to the post, please contact support.");
+        }
+        print("created :" + " " + folderId + " " + title + " " + text);
     }
 
     public static void main(String[] args) throws InterruptedException, SQLException {
